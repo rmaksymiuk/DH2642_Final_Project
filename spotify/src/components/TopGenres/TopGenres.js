7a2559824c7a7434578548878994955fe3fa535b
Sofiya Mitchell <53533612+sofiyamitchell@users.noreply.github.com>
2022-12-07 11:49:28 +0100

    top genres endpoint maybe

--- spotify/src/components/TopGenres/TopGenres.js
+++ spotify/src/components/TopGenres/TopGenres.js
@@ -1,4 +1,39 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const TOPGENRES_ENDPOINT="https://api.spotify.com/v1/recommendations/available-genre-seeds";
+
 export default function TopGenres(){
-    //We need to implement TopGenres tap here
-    //Appropriate API call needed
-}
+    const [token, setToken] = useState("");
+    const [data, setData] = useState({});
+  
+    useEffect(() => {
+      if (localStorage.getItem("accessToken")) {
+        setToken(localStorage.getItem("accessToken"));
+      }
+    }, []);
+  
+    function getTopGenres() {
+      axios
+        .get(TOPGENRES_ENDPOINT, {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        })
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  
+    return (
+      <>
+        <button onClick={getTopArtist}>Get Top Genres</button>
+        <br/>
+        {data?.items ? data.items.map((item) => <p>{item.name}</p>) : null}
+      </>
+    );
+  };
+  
