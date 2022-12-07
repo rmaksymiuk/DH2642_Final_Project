0e7fd48c09bc4a4d05656c429655232b28b924bf
Sofiya Mitchell <53533612+sofiyamitchell@users.noreply.github.com>
2022-12-07 10:49:04 +0100

    very minor changes

--- spotify/src/components/LogIn/LogIn.js
+++ spotify/src/components/LogIn/LogIn.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 const CLIENT_ID = "acb0d4e1978245188e48efd073036e0c";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL = "http://localhost:3001/";
+const REDIRECT_URL = "http://localhost:3000/";
 const SCOPES = [
   "user-top-read"
   //we need to add here what is needed to fetch api (refer to scopes section in Spotify Developer)
