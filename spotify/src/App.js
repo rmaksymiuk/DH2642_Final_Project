830103d85523c3468bbc71e55e337665c93c5e97
Roman Maksymiuk <romanmaksymiuk1@gmail.com>
2022-12-09 11:57:02 +0100

    Bugs fixed.

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import Navbar from "./components/Navbar/Navbar"
 import './App.css';
-import LogIn from './components/LogIn/LogIn';
 import TopTrack from './components/TopTracks/TopTracks';
 import TopArtist from './components/TopArtist/TopArtist';
 import TopGenres from './components/TopGenres/TopGenres';
@@ -15,13 +14,13 @@ function App() {
     <div className="App">
         <Navbar />
         <Routes>
-            <Route path="/" element={<App/>}/>
+            {/* <Route path="/" element={<App/>}/> */}
             <Route path="/topTracks" element={<TopTrack/>}/>
             <Route path="/topArtist" element={<TopArtist/>}/>
             <Route path="/topGenres" element={<TopGenres/>}/>
             <Route path="/listening" element={<Listening/>}/>
             <Route path="/login" element={<Login/>}/>
-        </Routes>
+        </Routes> 
     </div>
   );
 }
